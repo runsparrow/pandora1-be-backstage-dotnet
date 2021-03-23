16465793fc8e74b33b3eb79bc4e46415e9ced117
shengzw <shengzw@163.com>
2021-03-23 22:43:25 +0800

    RMS

---
+++ MISApi/Controllers/CMS/RMSController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MISApi.Controllers.HttpEntities;
+using MISApi.Entities.CMS;
+using MISApi.Services.CMS;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MISApi.Controllers.CMS
+{
+    /// <summary>
+    /// 文件处理
+    /// </summary>
+    public class RMSController : ControllerBase
+    {
+        #region RPC
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [Route("MIS/CMS/RMS/Upload", Name = "MIS_CMS_RMS_Upload")]
+        [HttpPost]
+        [Authorize]
+        public IActionResult Upload(DTO_RMS dto)
+        {
+            try
+            {
+                // DTO
+                if (dto != null)
+                {
+                    new RMSService().Upload(
+                        new RMS
+                        {
+                            Goods = dto,
+                            Member = new Member
+                            {
+                                Id = dto.MemberId,
+                                Name = dto.MemberName,
+                            }
+                        }
+                    );
+                    return new JsonResult(new DTO_Result
+                    {
+                        Result = true
+                    });
+                }
+                else
+                {
+                    return new JsonResult(new DTO_Result
+                    {
+                        Result = false,
+                        Message = "传入参数为空。"
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new DTO_Result
+                {
+                    Result = false,
+                    Message = $"上传记录创建失败。错误信息：{ex.Message}。"
+                });
+            }
+        }
+        /// <summary>
+        /// 下载文件
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [Route("MIS/CMS/RMS/Down", Name = "MIS_CMS_RMS_Down")]
+        [HttpPost]
+        [Authorize]
+        public IActionResult Down(DTO_RMS dto)
+        {
+            try
+            {
+                // DTO
+                if (dto != null)
+                {
+                    new RMSService().Upload(
+                        new RMS
+                        {
+                            Goods = dto,
+                            Member = new Member
+                            {
+                                Id = dto.MemberId,
+                                Name = dto.MemberName,
+                            }
+                        }
+                    );
+                    return new JsonResult(new DTO_Result
+                    {
+                        Result = true
+                    });
+                }
+                else
+                {
+                    return new JsonResult(new DTO_Result
+                    {
+                        Result = false,
+                        Message = "传入参数为空。"
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new DTO_Result
+                {
+                    Result = false,
+                    Message = $"上传记录创建失败。错误信息：{ex.Message}。"
+                });
+            }
+        }
+
+        #endregion
+
+        #region HttpEntities
+        /// <summary>
+        /// 
+        /// </summary>
+        public class DTO_RMS : Goods
+        {
+            /// <summary>
+            /// 会员Id
+            /// </summary>
+            [Description("会员Id")]
+            [JsonProperty("memberId")]
+            [DefaultValue(-1)]
+            public int MemberId { get; set; } = -1;
+            /// <summary>
+            /// 会员姓名
+            /// </summary>
+            [StringLength(255)]
+            [Description("会员姓名")]
+            [JsonProperty("memberName")]
+            [DefaultValue("")]
+            public string MemberName { get; set; } = "";
+        }
+
+        #endregion
+    }
+}
---
+++ MISApi/Entities/CMS/RMS.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MISApi.Entities.CMS
+{
+    /// <summary>
+    /// 文件系统
+    /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
+    [Serializable]
+    public partial class RMS
+    {
+        #region Not Mapped Property
+        /// <summary>
+        /// 上传记录
+        /// </summary>
+        [Description("上传记录")]
+        [JsonProperty("upload")]
+        [NotMapped]
+        public Upload Upload { get; set; }
+        /// <summary>
+        /// 下载记录
+        /// </summary>
+        [Description("下载记录")]
+        [JsonProperty("down")]
+        [NotMapped]
+        public Down Down { get; set; }
+        /// <summary>
+        /// 收款人
+        /// </summary>
+        [Description("商品")]
+        [JsonProperty("goods")]
+        [NotMapped]
+        public Goods Goods { get; set; }
+        /// <summary>
+        /// 会员
+        /// </summary>
+        [Description("会员")]
+        [JsonProperty("member")]
+        [NotMapped]
+        public Member Member { get; set; }
+        #endregion
+    }
+}
